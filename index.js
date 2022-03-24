2e54959d79e874314c42432db1b71c97a4e82c15
gilbert <eleazarbiboum@gmail.com>
2022-03-24 16:09:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+
+  let employee = {
+    name: "Sam",
+    streetAddress: "" }
+     
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   const newemployee = {...employee}
+   newemployee[key] = value;
+   return newemployee;
+  }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(newemployee, key, value){
+    
+    newemployee[key] = value;
+    return newemployee;
+  }
+  const newemployee = {streetAddress:'12 Broadway', name: 'Sam' }
+  //destructivelyUpdateEmployeeWithKeyAndValue(newemployee, 'streetAddress', '12 Broadway')
+ // assert.deepEqual(newemployee,{streetAddress: '12 Broadway', name: 'Sam' })ite your solution in this file!
+ function deleteFromEmployeeByKey(employee, key, value){
+    const newemployee ={...employee}
+    newemployee[key] = value;
+    delete newemployee.name;
+     return newemployee;
+ }
+ function destructivelyDeleteFromEmployeeByKey(newemployee, key, value){
+    newemployee[key] = value;
+     return newemployee;
+ }
